ffe63aa81f7cde0d2cd263f52bda88ccca1ae8cf
ValentinaRuiz00 <valentinaruiztapias1234@gmail.com>
2024-10-08 15:41:55 -0500

    Se agrega Modelo empleado

---
+++ WebApi/Models/EmployeeEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Models
+{
+    public class EmployeeEntity
+    {
+        public int IdEmployee { get; set; }
+        public string NameEmployee {  get; set; }
+        public string Position {  get; set; }
+        public string Office { get; set; }
+        public string Salary {  get; set; }
+
+    }
+}
